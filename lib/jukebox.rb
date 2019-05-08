69faf903d1cf96c8246fd8dfd0963eb0b32c6a81
Emily R <emilyroulund@gmail.com>
2019-05-08 12:52:29 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -38,6 +38,11 @@ def play (songs)
 end 
 end 
 
+  if (1..9).to_a.include?(song_to_play.to_i)
+    puts "Playing #{songs[song_to_play.to_i - 1]}"
+  elsif songs.include?(song_to_play)
+    puts "Playing #{song_to_play}"
+
 def exit_jukebox
   puts "Goodbye"
 end
